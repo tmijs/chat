48586891085fc45b569296d772ad56b300f8f95f
Jacob Foster <jfost3@gmail.com>
2025-06-20 05:24:43 -0500

    fix: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Client } from './Client';
 export { Client } from './Client';
-export type { ClientOptions, ClientEvents } from './Client';
+export type * from './Client';
 import { parseTag } from './irc';
-export { parseTag } from './irc';
+export * from './irc';
+export * from './twitch/events';
+export { default as Channel, ChannelPlaceholder } from './lib/Channel';
 
 export default {
 	Client,
